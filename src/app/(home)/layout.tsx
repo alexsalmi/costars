df927857151ec2c82faa36c9abecfa7ddba2cca6
Alex Salmi <39961974+alexsalmi@users.noreply.github.com>
2024-11-23 15:42:00 -0500

    v0.1.0 - Project Initialization (#3)
    * project scaffolding
    
    * Added home page and error pages
    
    * Add Vercel analytics
    
    * Fix menu bug
    
    * Fix image bug

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/layouts/footer";
+import Header from "@/components/layouts/header";
+
+export default function MenuLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer/>
+    </>
+  );
+}
