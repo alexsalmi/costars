0d630a26f577ea359c53c467c0d080f8411b183a
alexsalmi <alexjfsalmi@gmail.com>
2024-11-27 12:12:32 -0500

    Condensed card bug fix

--- src/components/presentation/card.tsx
+++ src/components/presentation/card.tsx
@@ -1,3 +1,4 @@
+import useGameState from '@/store/game.state';
 import '@/styles/components/card.scss'
 import Image from 'next/image';
 
@@ -8,14 +9,15 @@ interface ICSCardProps {
 }
 
 export default function CSCard({entity, reverse, target}: ICSCardProps) {
+	const {condensed} = useGameState();
   return (
 		<div className={`
 			card-container 
 			${reverse ? 'reverse ' : ''}
 			${target ? 'target ' : ''}
-			${entity.collapsed ? 'condensed' : ''}
+			${condensed ? 'condensed' : ''}
 		`}>
-			{ !entity.collapsed ? 
+			{ !condensed ? 
 				<Image className='card-image' 
 					src={`https://image.tmdb.org/t/p/w500${entity.image}`} alt={`Picture of ${entity.label}`} width={80} height={120}/>
 				: <></>
