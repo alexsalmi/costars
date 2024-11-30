fc8f245f2ff30419f8a40bb8192245c3bfefeb69
Alex Salmi <alexjfsalmi@gmail.com>
2024-11-30 10:37:20 -0500

    Update daily costars refresh

--- src/services/scheduler.service.ts
+++ src/services/scheduler.service.ts
@@ -1,13 +1,35 @@
-import { unstable_cache } from 'next/cache';
+'use server';
+import { revalidateTag, unstable_cache } from 'next/cache';
 import { randomPerson } from "./tmdb.service";
+import cron from 'node-cron';
 
 const daily: DailyCostars = {
-  target: await randomPerson(),
-  starter: await randomPerson()
-}
+  target: {} as GameEntity,
+  starter: {} as GameEntity
+};
+
+const refreshDailyCostars = async () => {
+  console.log("----- Refreshing Daily Costars -----")
+  daily.target = await randomPerson();
+  const targetSet = new Set(daily.target.credits);
+
+  do {
+    daily.starter = await randomPerson();
+  } while (daily.starter.credits!.some(c => targetSet.has(c)));
+
+  revalidateTag('daily_costars');
+};
+
+cron.schedule('0 0 * * *', async () => {
+  await refreshDailyCostars();
+});
 
 export const getDaily = unstable_cache(
   async () => { return daily; },
   [],
-  { revalidate: 86400}
+  {
+    tags: ['daily_costars']
+  }
 );
+
+refreshDailyCostars();
