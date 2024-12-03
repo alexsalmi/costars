faf98494b0c5c0a3226106559336ce61762ccc8c
alexsalmi <alexjfsalmi@gmail.com>
2024-12-02 21:29:42 -0500

    Set up cron job

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 
-export async function GET() {
+export async function GET(req: Request) {
+  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return NextResponse.json({}, {status: 401, statusText: "Unauthorized"});
+  }
+
   console.log("----- REVALIDATING -----");
 
   revalidateTag('daily_costars');
