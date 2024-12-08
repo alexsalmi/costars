e72eec74104d64aa80ed19526e0b9069386ab0e5
alexsalmi <alexjfsalmi@gmail.com>
2024-12-08 14:51:27 -0500

    Add back button to solution page

--- src/app/(app)/solution/[uuid]/page.tsx
+++ src/app/(app)/solution/[uuid]/page.tsx
@@ -1,3 +1,4 @@
+import CSBackButton from "@/components/inputs/back-button";
 import CardTrack from "@/components/presentation/card-track";
 import { getSolution } from "@/services/db.service";
 import '@/styles/pages/solution.scss';
@@ -12,9 +13,12 @@ export default async function CustomGame({ params }: ICustomGameProps) {
   const solution = await getSolution(uuid);
 
   return (
-    <div className="solution-page-container">
-      <h3>{"Here's"} my solution:</h3>
-      <CardTrack cards={solution}/>
-    </div>
+    <>
+      <CSBackButton/>
+      <div className="solution-page-container">
+        <h3>{"Here's"} my solution:</h3>
+        <CardTrack cards={solution}/>
+      </div>
+    </>
   );
 }
