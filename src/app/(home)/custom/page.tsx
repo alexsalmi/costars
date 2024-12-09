e4a612e266d84c8d56784ff30fef3968b16ebbc5
alexsalmi <alexjfsalmi@gmail.com>
2024-12-08 21:54:30 -0500

    Fix build error

--- src/app/(home)/custom/page.tsx
+++ src/app/(home)/custom/page.tsx
@@ -1,11 +1,11 @@
-import Home from "@/app/(home)/page";
+import Home from "@/app/(home)/home";
 import CSCustomGameModal from "@/components/game/custom-game-modal";
 
 export default function NewCustomGame() {
   return (
     <>
       <CSCustomGameModal />
-      <Home placeholder/>
+      <Home starter={{} as GameEntity} target={{} as GameEntity}/>
     </>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,40 +1,10 @@
-import Image from 'next/image';
-import CSButton from "@/components/inputs/button";
-import logo from '@/../public/costars_primary_logo.png';
-import '@/styles/pages/home.scss';
-import Link from 'next/link';
 import { getDailyCostars } from '@/services/cache.service';
-import CSDailyCostars from '@/components/presentation/daily-costars';
+import Home from './home';
 
-interface IHomeProps {
-  placeholder: boolean
-}
-
-export default async function Home({placeholder}: IHomeProps) {
-  const { target, starter } = placeholder ? 
-    {target: {} as GameEntity, starter: {} as GameEntity} :
-    await getDailyCostars();
+export default async function Index() {
+  const { target, starter } = await getDailyCostars();
 
   return (
-    <div className="home-page">
-      <Image
-        src={logo}
-        alt="Costars logo"
-        height={80}
-      />
-      <CSDailyCostars starter={starter} target={target}/>
-      <div className='home-page-button-container'>
-        <Link href="/custom">
-          <CSButton secondary>
-            Custom Game
-          </CSButton>
-        </Link>
-        <Link href="/unlimited">
-          <CSButton secondary>
-            Unlimited
-          </CSButton>
-        </Link>
-      </div>
-    </div>
+    <Home target={target} starter={starter}/>
   );
 }
