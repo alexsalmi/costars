f2fe6c644e88e87754ed2b192b93bfda6eb76c1c
alexsalmi <alexjfsalmi@gmail.com>
2024-12-12 18:33:22 -0500

    Fix modal sizing

--- src/components/presentation/details-modal.tsx
+++ src/components/presentation/details-modal.tsx
@@ -75,7 +75,7 @@ export default function CSDetailsModal({ isOpen, close, entity }: ICSDetailsModa
 	}
 
   return (
-		<CSModal isOpen={isOpen} close={close}>
+		<CSModal isOpen={isOpen} close={close} className='details-modal-container'>
 			<div className='details-modal-hero'>
 				<Image className='card-image' 
 					src={`https://image.tmdb.org/t/p/w185${entity.image}`} alt={`Picture of ${entity.label}`} 
