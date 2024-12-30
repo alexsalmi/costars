5bce446feafe27196f10ae30df99c08f853a268b
Alex Salmi <alexjfsalmi@gmail.com>
2024-12-30 10:45:53 -0500

    Fix Plausible bug

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { withPlausibleProxy } from 'next-plausible';
 
 const nextConfig: NextConfig = {
   images: {
@@ -24,4 +25,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+export default withPlausibleProxy()(nextConfig);
