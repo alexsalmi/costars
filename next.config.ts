96b5cb2016a9059480caa9921bb536aa917d0ce6
Alex Salmi <39961974+alexsalmi@users.noreply.github.com>
2025-03-09 15:03:18 -0400

    v1.0.6 - Working self hosted Plausible (#37)
    * Test Plausible in stage
    
    * Testing
    
    * Formattin
    
    * Fix plausible
    
    * Disable plausible in stage
    
    * Update version

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,22 @@ const nextConfig: NextConfig = {
   sassOptions: {
     silenceDeprecations: ['legacy-js-api'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/js/script.outbound-links.tagged-events.js',
+        destination: `${process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}/js/script.outbound-links.tagged-events.js`,
+      },
+      {
+        source: '/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
+        destination: `${process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}/api/event`,
+      },
+      {
+        source: '/proxy/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
+        destination: `${process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}/api/event`,
+      },
+    ];
+  },
 };
 
 export default withPlausibleProxy()(nextConfig);
